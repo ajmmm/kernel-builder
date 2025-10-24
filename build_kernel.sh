fa048671da69c87d03094dfbc696bf996e36be6c
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-10-24 11:31:45 +0100

    Tweaks so we can build an RC kernel. Add 6.18.0-rc2.

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,7 @@
 BASENAME=$(basename "${0}")
 BASEPATH=$(dirname "${0}")
 BASEPKG="kernel-ajm"
+BASE_PKG=$(echo "${BASEPKG}" | sed -e 's/-/_/g')
 BASESPEC="linux.spec"
 
 . "${BASEPATH}/build_lib.sh"
@@ -52,19 +53,25 @@ KERNEL_PATCH=$(echo "${KERNEL_VERSION}" | cut -f3- -d.)
 # Identify if this is a prerelease by exploding out the minor version.
 #
 
-KERNEL_PRERELEASE=$(echo "${KERNEL_MINOR}" | grep "rc")
+KERNEL_PRERELEASE=$(echo "${KERNEL_MINOR}" | cut -f2 -d-)
 
 #
 # If the PRERELEASE tag is not empty, this is not a formal released version
 #
 
 if [ ! -z "${KERNEL_PRERELEASE}" ]; then
-	KERNEL_CDN="https://git.kernel.org/torvalds/t"
+	KERNEL_CDN="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot"
 	KERNEL_TAR="linux-${KERNEL_VERSION}.tar"
 	KERNEL_TARGZ="${KERNEL_TAR}.gz"
 
 	# Generate a source URL
 	KERNEL_SRCURL="${KERNEL_CDN}/${KERNEL_TARGZ}"
+
+	# KERNEL_VERSION will be something like 6.18-rc2, with MAJOR at 6
+	# and MINOR at 18-rc2. We need to trasnslate this back to 6.18.0.
+	KERNEL_MINOR=$(echo "${KERNEL_MINOR}" | cut -f1 -d-)
+	KERNEL_PATCH="0"
+	KERNEL_VERSION="${KERNEL_MAJOR}.${KERNEL_MINOR}.${KERNEL_PATCH}"
 else
 	KERNEL_PRERELEASE="no"
 	KERNEL_CDN="https://cdn.kernel.org/pub/linux/kernel/v${KERNEL_MAJOR}.x"
@@ -130,8 +137,12 @@ echo "Rendering spec files ..."
 mkdir -p "${TMP_BUILDSPECS}"
 sed -e	"s/^Source0:.*$/Source0: ${KERNEL_SRCFILE}/g;
 		s/^NoSource:.*$//g;
+		s/^%define sublevel .*/%define sublevel ${KERNEL_PATCH}/g;
+		s/^%global pkg_version %{LKAver}$/%global pkg_version ${KERNEL_VERSION}/g;
 		s/kernel-lt/${BASEPKG}/g;
-		s/kernel-ml/${BASEPKG}/g;" \
+		s/kernel-ml/${BASEPKG}/g;
+		s/kernel_lt/${BASE_PKG}/g;
+		s/kernel_ml/${BASE_PKG}/g;" \
 		    "${ACTUAL_SPEC}" >"${TMP_BUILDSPEC}" || \
 			fatal "Rendering spec failed: ${ACTUAL_SPEC} > ${TMP_BUILDSPEC}"
 
---
+++ el9/linux-6.18.0/SOURCES/ChangeLog.md
@@ -0,0 +1,5 @@
+**Kernel 6.18**
+
+v0.rc2:
+Rebase to kernel 6.18-rc2
+- amd64 config is dead
