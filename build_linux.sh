9a1e48676343cfd8ff6ae6a783f921caa0c2eaf5
Alasdair McWilliam <alasdair.mcwilliam@isovalent.com>
2025-12-11 13:13:50 +0000

    Add --debug

--- build_linux.sh
+++ build_linux.sh
@@ -27,6 +27,7 @@ MK_CLEAN=0
 MK_PATH="${CURPATH}"
 MK_CONFIG=""
 MK_NEWDEF=0
+MK_DEBUG=0
 
 HELPTEXT=$(cat <<__EOF__
   --hash                Include git hash in build version
@@ -40,6 +41,7 @@ HELPTEXT=$(cat <<__EOF__
   --src <path>          Specifies linux src path
   --config <file>       Use file as config for build
   --newdef		Use defaults for new config symbols
+  --debug		Enforce debug kernel build
 __EOF__
 )
 
@@ -99,6 +101,10 @@ while (( "$#" )); do
 			MK_NEWDEF=1
 			shift ;;
 		
+		--debug)
+			MK_DEBUG=1
+			shift ;;
+		
 		--help|-h)
 			echo "${HELPTEXT}"
 			exit 0 ;;
@@ -121,13 +127,6 @@ trap cleanup EXIT
 # Switch to kernel source path (may be pwd)
 pushd "${MK_PATH}" || fatal "pushd(${MK_PATH}) failed"
 
-# Check we have a config
-if [ ! -z "${MK_CONFIG}" ]; then
-	[ -f ".config" ] && mv .config .config.bk
-	cp -f "${MK_CONFIG}" .config
-fi
-[ -f ".config" ] || fatal "No .config in pwd"
-
 # Defaults
 MK_FILE="Makefile"
 MK_NPROCS=$(${NPROC} --all)
@@ -142,6 +141,16 @@ else
 	GIT_BRANCH=${GIT_BRANCH//\//__}
 fi
 
+# Check the source tree is initialised, so we can run "make kernelrelease" to get build info.
+
+if ! ${MAKE} -s kernelrelease 1>/dev/null 2>&1; then
+	echo "*** Fresh tree, initialising with defconfig..."
+	${MAKE} defconfig || fatal "make defconfig failed, please init the tree"
+	${MAKE} prepare || fatal "make prepare failed, please init the tree"
+fi
+
+echo "--------------------------------------------------------------------------------"
+
 # Generate new version information.
 
 EXTRA_VERSION=$(grep -E "^EXTRAVERSION" "${MK_FILE}" | cut -f2 -d'=' | tr -d '[:space:]')
@@ -149,6 +158,9 @@ if [ "${MK_HASH}" -gt 0 ]; then
 	EXTRA_VERSION="${EXTRA_VERSION}.${GIT_HASH}.${GIT_BRANCH}"
 else
 	EXTRA_VERSION="${EXTRA_VERSION}.${GIT_BRANCH}"
+fi
+if [ "${MK_DEBUG}" -gt 0 ]; then
+	EXTRA_VERSION="${EXTRA_VERSION}.dbg"
 fi
 echo EXTRA_VERSION=${EXTRA_VERSION}
 
@@ -166,10 +178,42 @@ done
 MK_COMMAND="${MAKE} EXTRAVERSION=${EXTRA_VERSION} -j${MK_NPROCS}"
 
 echo "MK_HASH=${MK_HASH} MK_IMAGE=${MK_IMAGE} MK_MODULES=${MK_MODULES} MK_INSTALL=${MK_INSTALL} MK_CLEAN=${MK_CLEAN}"
-echo "MK_VERSION=${MK_VERSION} MK_NEWDEF=${MK_NEWDEF} MK_NPROCS=${MK_NPROCS}"
+echo "MK_VERSION=${MK_VERSION} MK_NEWDEF=${MK_NEWDEF} MK_NPROCS=${MK_NPROCS} MK_DEBUG=${MK_DEBUG}"
 echo "MK_COMMAND=${MK_COMMAND}"
 
+echo "--------------------------------------------------------------------------------"
 sleep 1
+
+# Check we have a config
+
+if [ ! -z "${MK_CONFIG}" ]; then
+	echo "*** Copying ${MK_CONFIG} into .config"
+	[ -f ".config" ] && mv .config .config.bk
+	cp -f "${MK_CONFIG}" .config
+	MK_PREPARE=1
+fi
+
+if [ "${MK_DEBUG}" -gt 0 ]; then
+	declare -A DBG_VALUES=(
+		["CONFIG_DEBUG_INFO"]=y
+		["CONFIG_DEBUG_INFO_SPLIT"]=n
+		["CONFIG_DEBUG_INFO_REDUCED"]=n
+		["CONFIG_DEBUG_INFO_DWARF4"]=y
+		["CONFIG_DEBUG_INFO_DWARF5"]=y
+		["CONFIG_DEBUG_INFO_LEVEL"]=3
+		["CONFIG_DEBUG_INFO_BTF"]=y
+		["CONFIG_GDB_SCRIPTS"]=y
+		["CONFIG_FRAME_POINTER"]=y
+		["CONFIG_STRIP_ASM_SYMS"]=n
+	)
+
+	CFG_SCRIPT="${MK_PATH}/scripts/config"
+
+	for flag in "${!DBG_VALUES[@]}"; do
+		echo "SET-VAL: ${flag}=${DBG_VALUES[$flag]}"
+		${CFG_SCRIPT} --set-val ${flag} ${DBG_VALUES[$flag]}
+	done
+fi
 
 if [ "${MK_PREPARE}" -gt 0 ]; then
 	if [ "${MK_NEWDEF}" -gt 0 ]; then
